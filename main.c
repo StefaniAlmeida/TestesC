565ac2211d7c9faed73ea95385b10083ec1684a2
StefaniAlmeida <almeidastefani099@gmail.com>
2017-09-17 11:46:27 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+  int cont;
+  int idade;
+  for(cont = 1; cont <= 5; cont = cont +1) {
+    printf("Digite sua idade: ");
+    scanf("%i", &idade);
+      if (idade < 18){
+      printf("Voçe é menor de idade!\n");
+      printf("\n");
+      
+      }
+      else{
+      printf("Voçe é maior de idade!\n");
+      printf("\n");
+      }
+      
+      
+    
+  }
+  return 0;
+}
